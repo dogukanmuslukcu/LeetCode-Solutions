a340995fd56f3850504acc880b2d572806c47141
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2021-01-30 12:04:03 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int nums[] = new int[]{ 0,1};
+              Arrays.sort(nums);
+              int x = 0;
+              boolean check1 = true;
+           boolean check2 = true;
+
+              for (int i = 0; i < nums.length; i++) {
+                  if (nums[i] == 0) {
+                      check1 = false;
+                }
+                   if (nums[i] == nums.length) {
+                       check2 = false;
+                   }
+               }
+             for (int i = 0; i < nums.length-1; i++) {
+                   if (nums[i] - nums[i+1] != -1) {
+                   x = nums[i]+1;
+                  }
+             }
+
+               if (check1) {
+             System.out.println(0);
+             } else if (check2) {
+                  System.out.println(nums.length);
+              } else {
+                   System.out.println(x);
+               }
+
+    }
+}
